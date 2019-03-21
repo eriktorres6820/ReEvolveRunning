1a70cb7e9de900e96b0c2a2327f96b2d577b3253
Erik <eriktorres6820@gmail.com>
2019-03-20 22:56:14 -0700

    Attempt fix 2

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 var express               = require("express"),
     app                   = express(),
     session               = require("express-session"),
@@ -7,18 +6,19 @@ var express               = require("express"),
     flash                 = require("connect-flash"),
     methodOverride        = require("method-override"),
     expressSanitizer      = require("express-sanitizer"),
-    request               = require('request'),
     //seedDB                = require("./seed"),
     User                  = require("./ModelsBare/runnerMD"),
     passport              = require("passport"),
-    localStrategy         = require("passport-local"),
-    runSpotRoutes         = require("./routesBare/runSpotsRT"),
+    localStrategy         = require("passport-local");
+
+var runSpotRoutes         = require("./routesBare/runSpotsRT"),
     commentRoutes         = require("./routesBare/commentsBareRT"),
     indexRoutes           = require("./routesBare/indexBareRT");
 
-var db  = mongoose.connection;
+var db                = mongoose.connection;
 var url = process.env.DATABASEURL || "mongodb://localhost:27017/runningsports";
 mongoose.connect(url, { useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public")); //console.log(__dirname);
@@ -28,12 +28,15 @@ app.use(flash());
 app.use(expressSanitizer());
 //seedDB(); // seed the db
 
+
+
 // PASSPORT CONFIGURATION
 app.use(session({
     secret: "Sucka Lucka Ding Dong",
     resave: false,
     saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new localStrategy(User.authenticate()));
@@ -59,7 +62,66 @@ app.use("/runSpots", runSpotRoutes);
 app.use("/runSpots/:id/comments", commentRoutes);
 
 //Start server
-app.listen(3000 || process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The Barefoot Server Started");
 });
+
+/*========== git basics =================================*/
+/*
+  Commands                                  Description
+  -----------------------------------------------------------------------
+* git init                                  Initializes git repository
+* git status                                Display current status of git
+* git add fileName.type                     Begin tracking designated files
+* git commit -m "Commit Message Here"       Commit added files with "message about file"
+* git log                                   Display all commits and CommitIds
+* git checkout CommitId                     Detached head to previous commit
+* git revert --no-commit CommitId..HEAD     Revert master to previous commit
+* q                                         Exit git log
+
+* To push to github
+    git push -u origin master
+
+/* NOTE files must be added everytime before committing*/
+
+
+
+/*============ Mongoose and mongo commands ================= */
+/*
+    $ mongo          // start program
+    show dbs         // show databases
+    use NAME         // switch to database called NAME
+    show collections // shows all the collections in the database
+    db.NAME.find()   // shows Key Valley pairs contained in the database
+    db.NAME.drop()   // Deletes collections
+    db.dropDatabase()// Deletes database
+*/
+
+/*=============== Heroku commands =========================*/
+/*
+* NOTE: Must add the following to package.json: "start": "node app.js"
+    See file for location
+* to login:
+    heroku login -i
+    
+* heroku create
+    create a new heroku app
+
+* git remote -v                             
+    Display remote to/from heroku
+    
+* git push heroku master                    
+    Pushes code to be run to heroku
+
+*heroku logs
+    Displays logs for specific app
+    
+*heroku run CODE 
+    Runs CODE
+
+*
+
+*/
+
+/*========== MongoDB Atlas ==============*/
 
